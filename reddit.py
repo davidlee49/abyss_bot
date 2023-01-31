c861374f2635eeb663005236ac88313243f337ed
davidli49 <dlee2812@gmail.com>
2023-01-30 17:57:47 -0800

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -1,14 +1,15 @@
 import praw
 import random
+from bot_reqs import REDDIT_ID, REDDIT_CS, REDDIT_PASS, REDDIT_CID
 
 
 def get_img():
-    reddit = praw.Reddit(client_id = "Rtg4xkG86_MqnCz3payAew",
-                         client_secret = "IhSJIBXcCs6m4NIN4AnnHAb2kK4dYw",
-                         username = "AbyssalAbyss",
-                         password = "prawAbyssal!2#",
-                         user_agent = "praw",
-                         check_for_async = False)
+    reddit = praw.Reddit(client_id=REDDIT_CID,
+                         client_secret=REDDIT_CS,
+                         username=REDDIT_ID,
+                         password=REDDIT_PASS,
+                         user_agent="praw",
+                         check_for_async=False)
 
 
     for submission in reddit.subreddit("confusingperspective").random_rising(limit=1):
